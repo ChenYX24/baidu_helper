72a251c6a74191ade48f47af3c862d589790a7a6
ChenYX24 <90974567+ChenYX24@users.noreply.github.com>
2023-09-20 15:11:00 +0800

    文件夹与文件操作（除了分享与上传）

--- baidu.py
+++ baidu.py
@@ -95,17 +95,15 @@ class BaiDuPan(object):
         return search_content
 
     def file_folder_share():
-        """文件夹
-          - 分享
-          - 删除
-          - 重命名
-          - 复制
-          - 移动
-          - 新建
-          - 上传
         """
+          - 分享
+        """
+        url = 'https://pan.baidu.com/share/set'
 
-    def file_folder_add(self, path, folder_name):
+    def folder_add(self, path, folder_name):
+        """
+        添加文件夹
+        """
         url = 'https://pan.baidu.com/api/create'
         payload = {
             'a': 'commit',
@@ -125,7 +123,10 @@ class BaiDuPan(object):
         response_content = json.loads(response.content.decode("utf-8"))
         print(response_content)
 
-    def file_folder_delete(self, delete_list):
+    def file_folder_delete(self, delete_list: list[str]):
+        """
+        删除文件夹或文件，传入一个list包含每一个要删除的路径
+        """
         url = 'https://pan.baidu.com/api/filemanager'
         payload = {
             'async': '2',
@@ -147,15 +148,191 @@ class BaiDuPan(object):
         response_content = json.loads(response.content.decode("utf-8"))
         print(response_content)
 
-    def file_delete():
-        """文件操作
-        - 分享
-        - 删除
-        - 重命名
-        - 复制
-        - 移动
-        - 上传
+    def file_folder_rename(self, paths, new_names):
         """
+        重命名
+        两个参数对应两个数组，记录路径和新名字
+        """
+        url = 'https://pan.baidu.com/api/filemanager'
+        payload = {
+            'async': '2',
+            'onnest': 'fail',
+            'opera': 'rename',
+            'bdstoken': 'c1bf432f74b461320ff1a46c5c0d38a0',
+            'clienttype': '0',
+            'app_id': '250528',
+            'web': '1',
+            'dp-logid': '35893700578085580069',
+        }
+        rename_list = [{"path": path, "newname": newname}
+                       for path, newname in zip(paths, new_names)]
+        rename_list_json = json.dumps(rename_list)
+        form_data = {
+            'filelist': rename_list_json,
+        }
+        response = requests.post(
+            url, headers=self.headers, data=form_data, params=payload)
+        response_content = json.loads(response.content.decode("utf-8"))
+        print(response_content)
+
+    def file_folder_copy(self, copy_list):
+        """
+        复制
+        数据结构为copy_list:[{"path":"","dest":"","newname":""}]
+        ondup两个可选参数,newcop,overwrite
+        如果newname为空则默认为原本的名字
+        """
+        url = 'https://pan.baidu.com/api/filemanager'
+        payload = {
+            'async': '2',
+            'onnest': 'fail',
+            'opera': 'copy',
+            'bdstoken': 'c1bf432f74b461320ff1a46c5c0d38a0',
+            'clienttype': '0',
+            'app_id': '250528',
+            'web': '1',
+            'dp-logid': '48507800776512390044',
+        }
+        for item in copy_list:
+            if not item.get('newname'):
+                item['newname'] = item['path'].split('/')[-1]
+        copy_list_json = json.dumps(copy_list)
+        form_data = {
+            'filelist': copy_list_json,
+        }
+        response = requests.post(
+            url, headers=self.headers, data=form_data, params=payload)
+        response_content = json.loads(response.content.decode("utf-8"))
+        print(response_content)
+
+    def file_folder_move(self, move_list):
+        """
+        移动
+        数据结构为move_list:[{"path":"","dest":"","ondup":"overwrite","newname":""}]
+        ondup三个可选参数,newcop,overwrite
+        如果newname为空则默认为原本的名字
+        """
+        url = 'https://pan.baidu.com/api/filemanager'
+        payload = {
+            'async': '2',
+            'onnest': 'fail',
+            'opera': 'move',
+            'bdstoken': 'c1bf432f74b461320ff1a46c5c0d38a0',
+            'clienttype': '0',
+            'app_id': '250528',
+            'web': '1',
+            'dp-logid': '48507800776512390044',
+        }
+        for item in move_list:
+            if not item.get('newname'):
+                item['newname'] = item['path'].split('/')[-1]
+        move_list_json = json.dumps(move_list)
+        form_data = {
+            'filelist': move_list_json,
+        }
+        response = requests.post(
+            url, headers=self.headers, data=form_data, params=payload)
+        response_content = json.loads(response.content.decode("utf-8"))
+        print(response_content)
+
+    def floder_push(self):
+        """
+        上传
+        """
+        preurl = 'https://pan.baidu.com/api/precreate'
+        payload = {
+            'bdstoken': 'c1bf432f74b461320ff1a46c5c0d38a0',
+            'app_id': '250528',
+            'channel': 'chunlei',
+            'web': '1',
+            'clienttype': '0',
+            'dp-logid': '48507800776512390044',
+        }
+        form_data = {
+            'path': '/test/kid1/files/file_content.json',
+            'autoinit': '1',
+            'target_path': '/test/kid1/',
+            'block_list': '["5910a591dd8fc18c32a8f3df4fdc1761"]',
+            'local_mtime': '1695178153',
+        }
+        """
+        响应：
+        {
+    "path": "\/test\/kid1\/files\/file_content.json",
+    "uploadid": "N1-MjE4LjE5LjQ2LjIxOToxNjk1MTkxNzQ4OjMyNjAxNzk5OTgwODY4OTQxOA==",
+    "return_type": 1,
+    "block_list": [
+        0
+    ],
+    "errno": 0,
+    "request_id": 326017999808689418
+}
+        """
+        super_url = 'https://xafj-ct10.pcs.baidu.com/rest/2.0/pcs/superfile2'
+
+        super_payload = {
+            'method': 'upload',
+            'logid': 'MTY5NTE5MTU1MzQ4MTAuMjE2MTU2ODI0MDQ3NTkxNzM=',
+            'app_id': '250528',
+            'channel': 'chunlei',
+            'web': '1',
+            'path': '%2Ftest%2Fkid1%2Ffiles%2Ffile_content.json',
+            'uploadid': 'N1-MjE4LjE5LjQ2LjIxOToxNjk1MTkxNzQ4OjMyNjAxNzk5OTgwODY4OTQxOA==',
+            'clienttype': '0',
+            'uploadsign': '0',
+            'partseq': '0',
+            'dp-logid': '48507800776512390044',
+        }
+        super_form_data = {
+            'file': '',  # 二进制内容
+        }
+        """
+        响应：{md5: "d1c709b3c4e693fef814a08dd123bd9e", partseq: "0", request_id: 6090625627965506000,…}
+        md5:"d1c709b3c4e693fef814a08dd123bd9e"
+        partseq:"0"
+        request_id:6090625627965506000
+        uploadid:"N1-MjE4LjE5LjQ2LjIxOToxNjk1MTkxNzQ4OjMyNjAxNzk5OTgwODY4OTQxOA=="
+        """
+        url = 'https://pan.baidu.com/api/create'
+
+        payload = {
+            'isdir': '0',
+            'bdstoken': 'c1bf432f74b461320ff1a46c5c0d38a0',
+            'app_id': '250528',
+            'channel': 'chunlei',
+            'web': '1',
+            'clienttype': '0',
+            'dp-logid': '48507800776512390044',
+        }
+        form_data = {
+            'path': '/test/kid1/files/file_content.json',
+            'size': '4901',
+            'uploadid': 'N1-MjE4LjE5LjQ2LjIxOToxNjk1MTkxNzQ4OjMyNjAxNzk5OTgwODY4OTQxOA==',
+            'target_path': '/test/kid1/',
+            'block_list': '["d1c709b3c4e693fef814a08dd123bd9e"]',
+            'local_mtime': '1695178153'
+        }
+        """
+        响应：
+            {
+                "category": 6,
+                "ctime": 1695191749,
+                "from_type": 1,
+                "fs_id": 371055329789533,
+                "isdir": 0,
+                "md5": "c5c5d6995o6cc45cd000f8f971bf6d62",
+                "mtime": 1695191749,
+                "path": "\/test\/kid1\/files\/file_content.json",
+                "server_filename": "file_content.json",
+                "size": 4901,
+                "errno": 0,
+                "name": "\/test\/kid1\/files\/file_content.json"
+            }
+        """
+        response = requests.post(
+            url, headers=self.headers, data=form_data, params=payload)
+        response_content = json.loads(response.content.decode("utf-8"))
+        print(response_content)
 
 
 if __name__ == "__main__":
@@ -165,6 +342,9 @@ if __name__ == "__main__":
     # file_content = baidu.readJson('./files/file_content.json')
     # search = baidu.search_server_filename('child', 1)
     # baidu.saveJson('./files/search.json', search)
-    baidu.file_folder_add('/test/', 'ttt')
-    baidu.file_folder_delete(
-        ["/test/child1/kid2/455", "/test/child1/kid2/test3.txt"])
+    # baidu.folder_add('/test/', 'ttt')
+    # baidu.file_folder_delete(
+    #     ["/test/child1/kid2/455", "/test/child1/kid2/test3.txt"])
+    # baidu.file_folder_rename(['/test/new_name','/test/test2.txt'], ['new_name2','new_name.txt'])
+    # baidu.file_folder_copy([{"path":"/test/new_name.txt","dest":"/test/kid2","newname":"new_name3.txt"},{"path":"/test/new_name.txt","dest":"/test/kid2"}])
+    # baidu.file_folder_move([{"path":"/test/kid1/new_name.txt","dest":"/test/kid1/child4","newname":"new_name.txt"}])
